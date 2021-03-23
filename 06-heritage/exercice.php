0c36ff1e7868b64e955acba788d0c08226a6721c
Matthieu Mota <matthieumota@gmail.com>
2021-03-23 14:54:36 +0100

    Fix exo

--- 06-heritage/exercice.php
+++ 06-heritage/exercice.php
@@ -7,21 +7,23 @@ On va devoir incrémenter l'immatriculation à chaque création de véhicule.
 Tous les attributs sont privés ou protégés.
 Un véhicule peut démarrer ou accélérer.
 La méthode accélérer renvoie la vitesse du véhicule (10 km/h)
-La vitesse du véhicule est un attribut idéalement.
-- Une moto possède TOUJOURS 2 roues.
-Une moto peut faire du 1 roue (Surcharge de accélérer) à 100 km/h
+Elle incrémente la vitesse de 10 à chaque fois qu'on l'appelle et s'arrête à la vitesse max du véhicule.
+La vitesse du véhicule est un attribut idéalement (et aussi la vitesse max)
 - Une voiture possède TOUJOURS 4 roues.
-Une voiture peut accélérer à 130 km/h
+Une voiture peut accélérer jusque 130 km/h
+- Une moto possède TOUJOURS 2 roues.
+Une moto peut faire du 1 roue (Surcharge de accélérer) à partir de 100 km/h jusque 140 km/h
+Une moto peut accélérer jusque 250 km/h
 - Un Camion possède un chargement (tableau), une capacité de chargement (à définir dans son constructeur)
 Par exemple, si le camion a une capacité de 5, il ne peut avoir que 5 éléments dans son tableau chargement
 On doit pouvoir faire $camion->addItem('Tomates');
 Si je dépasse la capacité, j'ai une erreur
 Un camion peut avoir X roues (constructeur)
-Un camion peut accélérer à 90 km/h
+Un camion peut accélérer jusque 90 km/h
 BONUS:
 Un camion peut attacher ou détacher une remorque (méthode)
 Le fait d'attacher LA remorque permet d'agrandir la capacité du camion (x2)
-Attention, si on détache la remarque, on perd le chargement qui est de trop
+Attention, si on détache la remarque, on perd le chargement qui est de trop (array_slice)
 BONUS:
 Un véhicule ne peut accélérer que s'il a déjà démarré.
 */
