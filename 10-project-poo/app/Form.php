68d65d5bbf872b9303c495d146d8fcec413e043e
Matthieu Mota <matthieumota@gmail.com>
2021-03-25 16:48:18 +0100

    Continue form

--- 10-project-poo/app/Form.php
+++ 10-project-poo/app/Form.php
@@ -4,6 +4,23 @@ namespace App;
 
 class Form {
     /**
+     * Contient les données du formulaire
+     */
+    private $data = [];
+
+    public function __construct($data = []) {
+        $this->data = $data;
+    }
+
+    /**
+     * Permet de récupérer les données du formulaire
+     */
+    public function get($key) {
+        // $this->data[$key] est en réalité $_POST[$key]
+        return $this->data[$key] ?? null;
+    }
+
+    /**
      * Génére un input en HTML.
      *
      * Le label est optionnel et le type est text par défaut.
@@ -11,10 +28,12 @@ class Form {
     public function input($name, $label = null, $type = 'text') {
         // On a un label custom ou bien le name avec une majuscule
         $label = $label ?? ucfirst($name);
+        // On récupére la value dans $this->data donc dans $_POST
+        $value = $this->get($name);
 
         return "
             <label for='$name'>$label</label>
-            <input type='$type' name='$name' id='$name' class='form-control' value=''>
+            <input type='$type' name='$name' id='$name' class='form-control' value='$value'>
         ";
     }
 
@@ -23,10 +42,42 @@ class Form {
      */
     public function textarea($name, $label = null) {
         $label = $label ?? ucfirst($name);
+        $value = $this->get($name);
 
         return "
             <label for='$name'>$label</label>
-            <textarea name='$name' id='$name' class='form-control'></textarea>
+            <textarea name='$name' id='$name' class='form-control'>$value</textarea>
         ";
+    }
+
+    /**
+     * Génére un select avec des options en HTML.
+     */
+    public function select($name, $label = null, $options = []) {
+        $label = $label ?? ucfirst($name);
+        $value = $this->get($name);
+
+        $list = '';
+        foreach ($options as $key => $option) {
+            // Permet de savoir si on met l'attribut selected ou pas
+            $selected = ($value == $key) ? 'selected' : '';
+            $list .= "<option value='$key' $selected>
+                $option
+            </option>";
+        }
+
+        return "
+            <label for='$name'>$label</label>
+            <select name='$name' id='$name' class='form-control'>
+                $list
+            </select>
+        ";
+    }
+
+    /**
+     * Permet de savoir si le formulaire est soumis.
+     */
+    public function isSubmit() {
+        return !empty($this->data);
     }
 }
--- 10-project-poo/public/index.php
+++ 10-project-poo/public/index.php
@@ -12,16 +12,31 @@ require __DIR__.'/../views/header.php';
 
 use App\Form;
 
-$form = new Form();
+$form = new Form($_POST);
 dump($form);
+dump($form->get('email'));
+
+// Sans l'objet, on fait ça
+$email = $_POST['email'] ?? null;
+// Avec l'objet, on fait ça
+$email = $form->get('email');
+
+if ($form->isSubmit()) {
+    // Envoi un email...
+    // Fais une requête en BDD...
+    echo $form->get('civility').' '.$form->get('email').' a envoyé un message: <br />';
+    echo $form->get('message');
+}
 
 ?>
 
 <form action="" method="post">
-    <?= $form->select('civility', 'Civilité', ['Mr', 'Mme']); ?>
+    <?= $form->select('civility', 'Civilité', ['Mr' => 'Mr', 'Mme' => 'Mme']); ?>
     <?= $form->input('email', null, 'email'); ?>
     <?= $form->input('telephone', 'Téléphone'); ?>
     <?= $form->textarea('message'); ?>
+
+    <button class="btn btn-primary">Envoyer</button>
 </form>
 
 <?php require __DIR__.'/../views/footer.php';
